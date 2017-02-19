d06c3af3e719e42bdc1a3054b458594954ac2f84
brownleeb <brownleeb702@gmail.com>
2017-02-19 13:25:37 -0500

    Update vacuum.py
    better handling of missing bottle

--- vacuum.py
+++ vacuum.py
@@ -1,14 +1,12 @@
 #!/usr/bin/python
-# Install to /usr/local/bin
+
 import time
-import syslog
+import syslog, os
 import RPi.GPIO as GPIO
 
 from subprocess import call
-# Import the ADS1x15 module.
 import Adafruit_ADS1x15
 
-# Create an ADS1115 ADC (16-bit) instance.
 adc = Adafruit_ADS1x15.ADS1115()
 RELAY_4 = 13
 PUMP = 37
@@ -17,20 +15,14 @@ GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(RELAY_4, GPIO.OUT)
 GPIO.setup(PUMP, GPIO.OUT)
-GPIO.output(PUMP, True)
-GPIO.output(RELAY_4, True)
 GAIN = 1
-vac= adc.read_adc(1, gain=GAIN)
-# Main loop.
-while (vac > 7100):
- try:
-    time.sleep(0.5)
-    vac = adc.read_adc(1, gain=GAIN)
-
- except Exception as ex:
-    template = "An exception of type {0} occured. Arguments:\n{1!r}"
-    message = template.format(type(ex).__name__, ex.args)
-    syslog.syslog(message)
-
+start=time.time()
+while ((adc.read_adc(1, gain=GAIN)) > 7100):
+   GPIO.output(PUMP, True)
+   GPIO.output(RELAY_4, True)
+   time.sleep(0.5)
+   if ((time.time() - start) > 10):
+      open('/srv/http/no_bottle','a').close()
+      break
 GPIO.output(RELAY_4, False)
 GPIO.output(PUMP, False)
